6a3aba7b4925b354e5167192d7cfe54381c9efab
nicolaslopezg <nicolas.lopez.g@usach.cl>
2020-11-19 14:53:01 -0300

    FEAT: Relacion 2 variables

--- Lab 1/Codigo_lab1.R
+++ Lab 1/Codigo_lab1.R
@@ -96,10 +96,81 @@ olor<- fisher.test(mushrooms$p, mushrooms$p.1,simulate.p.value=TRUE)
 print(olor$p.value)
 
 
+#Relacion entre 2 variables del estudio
 
+library(tidyverse)
+library(purrr)
 
+#Se renombran las columnas
+colnames(mushrooms) <- c("edibility", "cap_shape", "cap_surface", 
+                        "cap_color", "bruises", "odor", 
+                        "gill_attachement", "gill_spacing", "gill_size", 
+                        "gill_color", "stalk_shape", "stalk_root", 
+                        "stalk_surface_above_ring", "stalk_surface_below_ring", "stalk_color_above_ring", 
+                        "stalk_color_below_ring", "veil_type", "veil_color", 
+                        "ring_number", "ring_type", "spore_print_color", 
+                        "population", "habitat")
 
+#Se convierten las variables a factor
 
+mushrooms <- mushrooms %>% map_df(function(.x) as.factor(.x))
 
+#Se entregan niveles para poder trabajar los graficos 
 
+levels(mushrooms$edibility) <- c("edible", "poisonous")
+levels(mushrooms$cap_shape) <- c("bell", "conical", "flat", "knobbed", "sunken", "convex")
+levels(mushrooms$cap_color) <- c("buff", "cinnamon", "red", "gray", "brown", "pink", 
+                                "green", "purple", "white", "yellow")
+levels(mushrooms$cap_surface) <- c("fibrous", "grooves", "scaly", "smooth")
+levels(mushrooms$bruises) <- c("no", "yes")
+levels(mushrooms$odor) <- c("almond", "creosote", "foul", "anise", "musty", "none", "pungent", "spicy", "fishy")
+levels(mushrooms$gill_attachement) <- c("attached", "free")
+levels(mushrooms$gill_spacing) <- c("close", "crowded")
+levels(mushrooms$gill_size) <- c("broad", "narrow")
+levels(mushrooms$gill_color) <- c("buff", "red", "gray", "chocolate", "black", "brown", "orange", 
+                                 "pink", "green", "purple", "white", "yellow")
+levels(mushrooms$stalk_shape) <- c("enlarging", "tapering")
+levels(mushrooms$stalk_root) <- c("missing", "bulbous", "club", "equal", "rooted")
+levels(mushrooms$stalk_surface_above_ring) <- c("fibrous", "silky", "smooth", "scaly")
+levels(mushrooms$stalk_surface_below_ring) <- c("fibrous", "silky", "smooth", "scaly")
+levels(mushrooms$stalk_color_above_ring) <- c("buff", "cinnamon", "red", "gray", "brown", "pink", 
+                                             "green", "purple", "white", "yellow")
+levels(mushrooms$stalk_color_below_ring) <- c("buff", "cinnamon", "red", "gray", "brown", "pink", 
+                                             "green", "purple", "white", "yellow")
+levels(mushrooms$veil_type) <- "partial"
+levels(mushrooms$veil_color) <- c("brown", "orange", "white", "yellow")
+levels(mushrooms$ring_number) <- c("none", "one", "two")
+levels(mushrooms$ring_type) <- c("evanescent", "flaring", "large", "none", "pendant")
+levels(mushrooms$spore_print_color) <- c("buff", "chocolate", "black", "brown", "orange", 
+                                        "green", "purple", "white", "yellow")
+levels(mushrooms$population) <- c("abundant", "clustered", "numerous", "scattered", "several", "solitary")
+levels(mushrooms$habitat) <- c("wood", "grasses", "leaves", "meadows", "paths", "urban", "waste")
+
+mushrooms <- mushrooms %>% select(- veil_type)
+
+#Se observa si hay datos faltantes
+
+cat("\n","Elementos vacios en el dataset", "\n")
+
+map_dbl(mushrooms, function(.x) {sum(is.na(.x))})
+
+#Se puede ver que no hay datos faltantes
+
+#Grafico comestibilidad vs olor del hongo
+
+p5 <- ggplot(mushrooms, aes(x = edibility, y = odor, col = edibility)) + 
+  geom_jitter(alpha = 0.5) + 
+  scale_color_manual(breaks = c("edible", "poisonous"), 
+                     values = c("green", "red"))
+plot(p5)
+
+#Grafico textura de la copa vs color de la copa del hongo
+
+p6 <- ggplot(mushrooms, aes(x = cap_surface, y = cap_color, col = edibility)) + 
+  geom_jitter(alpha = 0.5) + 
+  scale_color_manual(breaks = c("edible", "poisonous"), 
+                     values = c("green", "red"))
+plot(p6)
+
+#Con el gráfico anterior es posible ver la relación entre dos variables y como estas se relacionan
 
