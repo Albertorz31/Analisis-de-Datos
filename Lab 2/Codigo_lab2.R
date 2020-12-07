6980822fae9637d1e1283578ad8a882b0c386461
albertorz31 <alberto.rodriguez.z@usach.cl>
2020-12-07 18:13:37 -0300

    MCA

--- Lab 2/Codigo_lab2.R
+++ Lab 2/Codigo_lab2.R
@@ -5,6 +5,8 @@
 library(dplyr) 
 library(ggplot2)
 library(ggpubr)
+library(factoextra)
+library(FactoMineR)
 url<-"https://raw.githubusercontent.com/Albertorz31/Analisis-de-Datos/main/agaricus-lepiota.data"
 
 # Leemos los datos
@@ -19,10 +21,23 @@ set.seed(88)
 
 #Eliminamos el atributo "classes" o como sale escrito "p".
 mushrooms$p=NULL
-#Eliminamos todas las filas donde el atributo stalk.root tiene datos faltantes
-mushrooms <- mushrooms[which(mushrooms$e.1 != '?'),]
-# Eliminamos atributo innecesario (solo tiene un valor posible)
+
+#Usamos summary para calcular la frecuencia de cada variables categórica
+summary(mushrooms)
+# Se puede observar que el atributo “veil-type” o p.2 
+#solo tiene un posible valor, entonces se elimina
+#Eliminamos atributo innecesario (solo tiene un valor posible)
 mushrooms$p.2 <- NULL
+
+#Eliminamos todas las filas donde el atributo stalk.root tiene datos faltantes
+#mushrooms <- mushrooms[which(mushrooms$e.1 != '?'),]
+
+#Realizamos análisis de componentes múltiple
+res.mca <- MCA(mushrooms, graph = FALSE)
+#Para visualizar la proporción de variaciones retenidas por las diferentes dimensiones.
+eig.val <- get_eigenvalue(res.mca)
+#Para visualizar los porcentajes de inercia explicados por cada dimensión de MCA
+fviz_screeplot(res.mca, addlabels = TRUE, ylim = c(0, 45))
 
 
 
