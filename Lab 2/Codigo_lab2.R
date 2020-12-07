bebd5136dd8e559a21daffc2efdc058e8d38d07c
albertorz31 <alberto.rodriguez.z@usach.cl>
2020-12-06 23:38:43 -0300

    filtro atributos

--- Lab 2/Codigo_lab2.R
+++ Lab 2/Codigo_lab2.R
@@ -9,3 +9,26 @@ url<-"https://raw.githubusercontent.com/Albertorz31/Analisis-de-Datos/main/agari
 
 # Leemos los datos
 mushrooms <- read.table(url, header = TRUE, sep = ",")
+
+#Semilla: se usa porque como se eligen los centroides al azar, usamos esta funcion para que obtengamos siempre 
+#los mismo resultadoss
+set.seed(88)
+
+
+######PRE-PROCESAMIENTO######
+
+#Eliminamos el atributo "classes" o como sale escrito "p".
+mushrooms$p=NULL
+#Eliminamos todas las filas donde el atributo stalk.root tiene datos faltantes
+mushrooms <- mushrooms[which(mushrooms$e.1 != '?'),]
+# Eliminamos atributo innecesario (solo tiene un valor posible)
+mushrooms$p.2 <- NULL
+
+
+
+
+
+
+
+
+
